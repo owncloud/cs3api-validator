25cefd6a82ca59a67bea88e678d9191592f58018
Michael Barz <mbarz@owncloud.com>
2021-12-30 17:44:00 +0100

    Add login, grpc client and first request

--- cs3api-validator_test.go
+++ cs3api-validator_test.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"testing"
 
 	gateway "github.com/cs3org/go-cs3apis/cs3/gateway/v1beta1"
+	rpc "github.com/cs3org/go-cs3apis/cs3/rpc/v1beta1"
+	providerv1beta1 "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
+	ctxpkg "github.com/cs3org/reva/pkg/ctx"
+	"github.com/cs3org/reva/pkg/rgrpc/todo/pool"
 	"github.com/cucumber/godog"
 	"github.com/cucumber/godog/colors"
 	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/metadata"
 )
 
 var opts = godog.Options{
@@ -24,12 +32,57 @@ type User struct {
 }
 
 type FeatureContext struct {
-	Client gateway.GatewayAPIClient
-	Users  []User
+	Client   gateway.GatewayAPIClient
+	Users    map[string]User
+	Response interface{}
+}
+
+func (f *FeatureContext) loginUser(authType string, user string, pass string) error {
+	req := &gateway.AuthenticateRequest{
+		Type:         authType,
+		ClientId:     user,
+		ClientSecret: pass,
+	}
+
+	ctx := context.Background()
+	res, err := f.Client.Authenticate(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	if res.Status.Code != rpc.Code_CODE_OK {
+		return formatError(res.Status)
+	}
+	f.Users[user] = User{ RevaToken: res.Token}
+	return nil
+}
+
+func formatError(status *rpc.Status) error {
+	return fmt.Errorf("error: code=%+v msg=%q support_trace=%q", status.Code, status.Message, status.Trace)
+}
+
+func (f *FeatureContext) getAuthContext(u string) context.Context {
+	ctx := context.Background()
+	ctx = ctxpkg.ContextSetToken(ctx, f.Users[u].RevaToken)
+	ctx = metadata.AppendToOutgoingContext(ctx, ctxpkg.TokenHeader, f.Users[u].RevaToken)
+	return ctx
 }
 
 func (f *FeatureContext) userHasCreatedAPersonalSpace(user string) error {
-	return godog.ErrPending
+	var err error
+	ctx := f.getAuthContext(user)
+	f.Response, err = f.Client.CreateStorageSpace(ctx, &providerv1beta1.CreateStorageSpaceRequest{Type: "personal", Name: "Einstein"})
+	if err != nil {
+		return err
+	}
+	if resp, ok := f.Response.(*providerv1beta1.CreateStorageSpaceResponse); ok {
+		if resp.Status.Code != rpc.Code_CODE_OK {
+			return formatError(resp.Status)
+		}
+		return assertExpectedAndActual(assert.Equal, resp.StorageSpace.Name, "Einstein")
+	} else {
+		return fmt.Errorf("did not receive a valid response: %v", resp)
+	}
 }
 
 func (f *FeatureContext) userListsAllAvailableSpaces(user string) error {
@@ -44,7 +97,17 @@ func InitializeTestSuite(sc *godog.TestSuiteContext) {
 }
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
+	var err error
 	f := &FeatureContext{}
+	f.Users = make(map[string]User)
+	f.Client, err = pool.GetGatewayServiceClient("localhost:9142")
+	if err != nil {
+		print("error")
+	}
+	err = f.loginUser("basic", "einstein", "relativity")
+	if err != nil {
+		print(fmt.Sprintf("Error during login: %s", err.Error()))
+	}
 	ctx.Step(`^user "([^"]*)" has created a personal space$`, f.userHasCreatedAPersonalSpace)
 	ctx.Step(`^user "([^"]*)" lists all available spaces$`, f.userListsAllAvailableSpaces)
 	ctx.Step(`^one personal space should be listed in the response$`, f.onePersonalSpaceShuoldBeListedInTheResponse)
@@ -67,4 +130,35 @@ func TestMain(m *testing.M) {
 	}
 
 	os.Exit(status)
+}
+
+// assertExpectedAndActual is a helper function to allow the step function to call
+// assertion functions where you want to compare an expected and an actual value.
+func assertExpectedAndActual(a expectedAndActualAssertion, expected, actual interface{}, msgAndArgs ...interface{}) error {
+	var t asserter
+	a(&t, expected, actual, msgAndArgs...)
+	return t.err
+}
+
+type expectedAndActualAssertion func(t assert.TestingT, expected, actual interface{}, msgAndArgs ...interface{}) bool
+
+// assertActual is a helper function to allow the step function to call
+// assertion functions where you want to compare an actual value to a
+// predined state like nil, empty or true/false.
+//func assertActual(a actualAssertion, actual interface{}, msgAndArgs ...interface{}) error {
+//	var t asserter
+//	a(&t, actual, msgAndArgs...)
+//	return t.err
+//}
+//
+//type actualAssertion func(t assert.TestingT, actual interface{}, msgAndArgs ...interface{}) bool
+
+// asserter is used to be able to retrieve the error reported by the called assertion
+type asserter struct {
+	err error
+}
+
+// Errorf is used by the called assertion to report an error
+func (a *asserter) Errorf(format string, args ...interface{}) {
+	a.err = fmt.Errorf(format, args...)
 }
--- go.mod
+++ go.mod
@@ -3,15 +3,34 @@ module cs3api-validator
 go 1.17
 
 require (
+	github.com/cs3org/go-cs3apis v0.0.0-20211214102128-4e8745ab1654
+	github.com/cs3org/reva v1.17.0
 	github.com/cucumber/godog v0.12.2
 	github.com/spf13/pflag v1.0.5
+	github.com/stretchr/testify v1.7.0
+	google.golang.org/grpc v1.42.0
 )
 
 require (
 	github.com/cucumber/gherkin-go/v19 v19.0.3 // indirect
 	github.com/cucumber/messages-go/v16 v16.0.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.0 // indirect
 	github.com/hashicorp/go-memdb v1.3.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/mileusna/useragent v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.27.0 // indirect
+	go.opentelemetry.io/otel v1.2.0 // indirect
+	go.opentelemetry.io/otel/exporters/jaeger v1.2.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.2.0 // indirect
+	go.opentelemetry.io/otel/trace v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sys v0.0.0-20210921065528-437939a70204 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
